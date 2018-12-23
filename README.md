e7c4f0995e949d808d18eefefab597b54e5275cb
EarsKilla <EarsKilla@users.noreply.github.com>
2018-12-23 22:46:27 +0300

    Queue restarts amount

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
 # SteamSales-Helper
-* Clicks `Next in Queue` button in your recomendations queue.
-* Skips age request.
-* Restarts recomendation queue.
-
-## To do
-* ~Skip content confirmation~ No needed. Steam changed button to `Next in Queue`
+* Clicks `Next in Queue` button in your recommendations queue.
+* Skips all weird questions like age question.
+* You can set restarts amount of recommendation queue via `Store` > `Auto explore` and this script will restart queue automatically.
 
 ## Installation
 1. Install [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) (for Chrome) or [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) (for Firefox)
-2. Go [here](https://github.com/EarsKilla/SteamSales-Helper/raw/master/SSh.user.js)
+2. Go [here](https://github.com/EarsKilla/SteamSales-Helper/raw/Extended/SSh.user.js)
 3. Click `Install`
 
 ## Credits
-* [DEMENT0R](https://github.com/DEMENT0R/)
+* [DEMENT0R](https://github.com/DEMENT0R/) for the base
 ##
 > By default, the script will auto-update roughly every 24 hours. You can force an update by re-installing using the link above.
--- SSh.user.js
+++ SSh.user.js
@@ -4,8 +4,8 @@
 // @description Simple button Clicker
 // @version 1.0
 // @author DEMENT0R + EarsKilla#0697
-// @downloadURL https://github.com/EarsKilla/SteamSales-Helper/raw/master/SSh.user.js
-// @updateURL https://github.com/EarsKilla/SteamSales-Helper/raw/master/SSh.user.js
+// downloadURL https://github.com/EarsKilla/SteamSales-Helper/raw/master/SSh.user.js
+// updateURL https://github.com/EarsKilla/SteamSales-Helper/raw/master/SSh.user.js
 // @license GNU v3
 // @noframes
 // @match        http://store.steampowered.com/*
@@ -15,11 +15,90 @@
 
 (function() {
     'use strict';
-    setTimeout(function(){
+
+    function V_GetIntCookie(name, defValue) {
+        var _def = parseInt(defValue);
+        if (isNaN(_def))
+            _def = 0;
+
+        var _val = parseInt(V_GetCookie(name));
+        if (isNaN(_val))
+            return _def;
+        return _val;
+    }
+
+    function setExploreLoops(count) {
+        var _count = parseInt(count);
+        if (isNaN(_count))
+            _count = 3;
+
+        V_SetCookie('sshjs_countMax', _count, 360);
+    }
+
+    function getExploreLoops(defCount = 3) {
+        return V_GetIntCookie('sshjs_countMax', defCount);
+    }
+
+    function getExplored() {
+        return V_GetIntCookie('sshjs_count', 0);
+    }
+
+    function setExplored(val) {
+        var _val = parseInt(val);
+        if (isNaN(_val))
+            _val = 0;
+
+        V_SetCookie('sshjs_count', _val, 360);
+    }
+
+    function gabenSendNudes() {
+        var sshDialog = ShowPromptDialog('Explore', 'Loops count:', 'Go', 'Cancel');
+        var sshInput = sshDialog.m_$Content[0].childNodes[1].children[0].childNodes[0].childNodes[0].childNodes[1].childNodes[0];
+        if (!sshInput) {
+            console.log('Cannot find input field! this is very bad :(');
+            return;
+        }
+        sshInput.setAttribute('type', 'number');
+        sshInput.value = getExploreLoops();
+
+        sshDialog.done(function () {
+            setExplored(0);
+            setExploreLoops(sshInput.value);
+            window.location.href = "https://store.steampowered.com/explore/startnew";
+        });
+    }
+
+    function updateStoreMenu() {
+        var store_subm = document.getElementsByClassName('submenu_store');
+        if (!store_subm) {
+            console.log('Cannot find element with submenu_store class :(');
+            return;
+        }
+
+        var store = store_subm[1],
+            extLink = document.createElement('a');
+
+        extLink.setAttribute('class', 'submenuitem');
+        extLink.setAttribute('href', '#');
+        extLink.onclick = function(e) { gabenSendNudes(); };
+        extLink.innerText = 'Auto explore';
+
+        store.appendChild(extLink);
+    }
+
+    updateStoreMenu();
+
+    setTimeout(function () {
         if (window.location.href.includes(".com/agecheck/")) {
             document.getElementById("next_in_queue_form").submit();
         } else if (window.location.href.includes(".com/explore")) {
-            window.location.href = "https://store.steampowered.com/explore/startnew";
+            var lCount = getExplored(),
+                lcountMax = getExploreLoops(0) - 1;
+
+            if (lCount < lcountMax) {
+                setExplored(lCount + 1);
+                window.location.href = "https://store.steampowered.com/explore/startnew";
+            }
         }
         else {
             document.querySelector('.next_in_queue_content').click();
